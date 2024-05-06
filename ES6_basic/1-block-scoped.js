25f7cea03ff27b16aa208bee4680ec9f9e009f77
Ashley <ajchapman777@gmail.com>
2024-05-06 23:57:50 +0000

    1-block-scoped.js

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+// 1-block-scoped.js
+
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
+
+  return [task, task2];
+}
