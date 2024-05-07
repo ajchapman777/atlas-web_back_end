2a08386d4432f788cb00bc87c760cf928d3022b6
Ashley <ajchapman777@gmail.com>
2024-05-07 05:11:22 +0000

    4-update_grade_by_city.js

---
+++ ES6_data_manipulation/4-main.js
@@ -0,0 +1,18 @@
+[
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+    grade: 86
+  },
+  { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 97 }
+]
+[
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+    grade: 'N/A'
+  },
+  { id: 5, firstName: 'Serena', location: 'San Francisco', grade: 97 }
+]
