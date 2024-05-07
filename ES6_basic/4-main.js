ddb80611ea3712ff5fd7a8aaf8fc30c8d1532f26
Ashley <ajchapman777@gmail.com>
2024-05-07 00:17:07 +0000

    4-rest-parameters.js

---
+++ ES6_basic/4-main.js
@@ -0,0 +1,5 @@
+// 4-main.js
+import returnHowManyArguments from './4-rest-parameter.js';
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
