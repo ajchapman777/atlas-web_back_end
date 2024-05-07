541cd36aeabc6db489508ffa70827a01f8b55af7
Ashley <ajchapman777@gmail.com>
2024-05-07 05:31:38 +0000

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
-function cleanSet(set, startString) {
+function cleanSet(set, startString = '') {
   // Convert the Set to an array, filter values that start with startString,
   // then map them to their substring starting from startString length,
   // and finally join them using '-'
   return [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length))
+    .filter(value => value.startsWith(startString))
+    .map(value => value.substring(startString.length))
     .join('-');
 }
 
