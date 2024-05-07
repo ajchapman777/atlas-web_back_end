fdcaffa7c6efd487dbd7b90017847410da17af5f
Ashley <ajchapman777@gmail.com>
2024-05-07 00:09:20 +0000

    1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -5,9 +5,6 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    // Use the variables directly without reassigning them
-    task = task; // This line is not necessary and can be removed
-    task2 = task2; // This line is not necessary and can be removed
   }
 
   return [task, task2];
