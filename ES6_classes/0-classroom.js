a94774b2a23b18fae1d4dbae4c1e7e182c99e40c
Ashley <ajchapman777@gmail.com>
2024-05-07 02:16:46 +0000

    0-classroom.js

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,7 @@
+// 0-classroom.test.js
+import ClassRoom from "./0-classroom.js";
+
+test("Initialize an instance of ClassRoom set to 100", () => {
+  const room = new ClassRoom(100);
+  expect(room._maxStudentsSize).toBe(100);
+});
