2cae76bd4447f4adff13e46799fd67b244adec45
Ashley <ajchapman777@gmail.com>
2024-05-07 03:31:40 +0000

    4-pricing.js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,18 +1,19 @@
 // 4-pricing.js
-import Currency from './3-currency';
+
+import Currency from './3-currency.js';
 
 export default class Pricing {
   constructor(price, currency) {
     if (!(currency instanceof Currency)) {
       throw new TypeError('Currency must be an instance of Currency');
     }
-    this._price = price;
+    this._amount = price;
     this._currency = currency;
   }
 
-  // Getter for price
-  get price() {
-    return this._price;
+  // Getter for amount
+  get amount() {
+    return this._amount;
   }
 
   // Getter for currency
@@ -22,6 +23,6 @@ export default class Pricing {
 
   // Method to display the full price
   displayFullPrice() {
-    return `${this._price} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 }
