2ded0d4fc0d19da886141e1b08d847b076f6437d
Ashley <ajchapman777@gmail.com>
2024-05-07 02:55:58 +0000

    2-hbtn_course.js

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,63 @@
+// 2-hbtn_course.js
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name!== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length!== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) ||!students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(value) {
+    if (typeof value!== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(value) {
+    if (typeof value!== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(value) {
+    if (!Array.isArray(value) ||!value.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+
+  // Method to display the course details
+  displayCourseDetails() {
+    return `Course: ${this._name}, Length: ${this._length}, Students: ${this._students.join(', ')}`;
+  }
+}
