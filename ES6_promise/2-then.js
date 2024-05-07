40201b93c14eb61460dd724880e68a8a6de6dc97
Ashley <ajchapman777@gmail.com>
2024-05-07 06:10:35 +0000

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 function handleResponseFromAPI(promise) {
   promise.then(
-    (response) => {
+    () => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     },
-    (error) => {
+    () => {
       console.log('Got a response from the API');
       return new Error();
     }
