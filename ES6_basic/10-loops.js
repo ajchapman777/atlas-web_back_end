cb4dd2b6f45fabccb44e881cf79a63d837cee013
Ashley <ajchapman777@gmail.com>
2024-05-07 00:38:46 +0000

    10-main.js

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+// 10-loops.js
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    array[array.indexOf(value)] = appendString + value;
+  }
+
+  return array;
+}
---
+++ ES6_basic/10-main.js
@@ -0,0 +1,4 @@
+// 10-main.js
+import appendToEachArrayValue from './10-loops.js';
+
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
