7c9e14ef772078a597c829f701d91cc77e5d82e2
ajchapman777 <ajchapman777@gmail.com>
2024-05-09 03:31:12 +0000

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -10,7 +10,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     return results.map(result => ({
       status: result.status,
       value:
-        result.status === 'fulfilled' ? result.value : result.reason.message,
+        result.status === 'fulfilled' ? result.value : new Error(result.reason),
     }));
   });
 }
